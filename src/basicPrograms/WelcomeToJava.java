fb57b6fc0731fd4b3e9bc1950b13cb8f025d0259
srinivas <srinuamujuri123@gmail.com>
2023-02-28 10:57:29 +0530

    firstCommit

---
+++ src/basicPrograms/WelcomeToJava.java
@@ -0,0 +1,7 @@
+package basicPrograms;
+
+public class WelcomeToJava {
+public static void main(String[] args) {
+	System.out.println("welcome to java programing class");
+}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author vj
+ *
+ */
+module JavaPrograms {
+}
