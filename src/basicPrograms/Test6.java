f251235eb5436f37fd968dae2014c018daf54476
srinivas <srinuamujuri123@gmail.com>
2023-02-28 16:33:25 +0530

    4

--- src/basicPrograms/Test6.java
+++ src/basicPrograms/Test6.java
@@ -3,6 +3,7 @@ package basicPrograms;
 public class Test6 {
 	public static void main(String[] args) {
 		System.out.println(sumOfnNaturalNumbers());
+		System.out.println(sumOfDigitsOfGivenNumber());
 	}
 
 	static String sumOfnNaturalNumbers() {
@@ -11,5 +12,16 @@ public class Test6 {
 			sum += i;
 		}
 		return "sum of " + number + " natural numbers is " + sum;
+	}
+
+	static String sumOfDigitsOfGivenNumber() {
+		int number = 156, sum = 0, copy = number;
+		while (number > 0) {
+			int reminder = number % 10;
+			sum += reminder;
+			number = number / 10;
+
+		}
+		return "Sum of digits of " + copy + " number is " + sum;
 	}
 }
---
+++ src/basicPrograms/Test7.java
@@ -0,0 +1,25 @@
+package basicPrograms;
+
+public class Test7 {
+	public static void main(String[] args) {
+		int numberOne = 10, numberTwo = 20;
+		System.out.println("---without 3rd variable---");
+		System.out.println("Before swapping numberone value is " + numberOne);
+		System.out.println("Before swapping numbertwo value is " + numberTwo);
+		numberOne += numberTwo;
+		numberTwo = numberOne - numberTwo;
+		numberOne -= numberTwo;
+		System.out.println("After swapping numberone value is " + numberOne);
+		System.out.println("After swapping numbertwo value is " + numberTwo);
+		
+		System.out.println("---with 3rd variable---");
+		int numberThree = numberOne;
+		System.out.println("Before swapping numberone value is " + numberOne);
+		System.out.println("Before swapping numbertwo value is " + numberTwo);
+		numberOne = numberTwo;
+		numberTwo = numberThree;
+		System.out.println("After swapping numberone value is " + numberOne);
+		System.out.println("After swapping numbertwo value is " + numberTwo);
+		
+	}
+}
